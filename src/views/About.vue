937c72f858e905627181beb16af239814f7571cb
hedong <414123754@qq.com>
2021-10-13 19:10:01 +0800

    添加脚本

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,24 @@
     <h1>bug_1</h1>
 
     <div>
-      <p>title</p>
       <div>如果多人协作开发，别人有提交过代码，必须先git pull到本地，如果有冲突解决完冲突，然后先提交到本地，再git push到远端库。</div>
     </div>
+
+    <p>hello wolld!</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
