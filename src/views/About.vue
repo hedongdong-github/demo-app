083a54dadec855769e584f7d932db58f1179b2cf
hedong <414123754@qq.com>
2021-10-14 09:16:56 +0800

    删除无效的模板

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-
-    <h1>bug_1</h1>
-
-    <div>
-      <div>如果多人协作开发，别人有提交过代码，必须先git pull到本地，如果有冲突解决完冲突，然后先提交到本地，再git push到远端库。</div>
-    </div>
-
-    <p>hello wolld!</p>
     <p>{{ msg }}</p>
   </div>
 </template>
